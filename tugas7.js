8375e854b11cf0b881a1dc660f95a60a342eb69c
Haikal Rafli <haikalraflis35@gmail.com>
2021-12-04 21:12:18 +0700

    TUGAS 7 JS
    tugas 7 javascript

---
+++ tugas7.js
@@ -0,0 +1,8 @@
+var tinggiBadan = [150, 155, 160, 165, 170, 175, 180, 185, 190, 195];
+for (let i = 0; i < tinggiBadan.length; i++) {
+  console.log(tinggiBadan[i]);
+}
+console.log('');
+for (let j of tinggiBadan) {
+  console.log(j);
+}
